6b0c87c973f91869d3cd1615e03b0e94ff8026db
Rizkisa31 <rizkisaputra4343@gmail.com>
2023-10-26 09:50:42 +0700

    first commit

---
+++ Tugas_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mahasiswa</title>
+</head>
+<style>
+    .a{
+        text-decoration: none;
+    }
+</style>
+<body>
+    <p align="center">Selamat Datang Di Website Rpl Polibang Jepara</p><br>
+    <h1 align="center">Ayo Kita Belajar Web Developer Bersama</h1>
+    <h2 align="center">Nama Mahasiswa : Rizki Wibowo Saputro</h2>
+    <h3 align="center">Alamat : Bugel Kedung Jepara</h3>
+    <p align="center"><a href="http://polibang.ac.id">Link Web Polibang</a></p>
+    <p align="center"><a href="https://www.w3shools.com">Link Web Belajar Pemrograman</a></p>
+</body>
+</html>
---
+++ Tugas_4.0/Tugas_4.php
@@ -0,0 +1,15 @@
+<html>
+    <head>        
+    </head>
+    <FRAMESET COLS="20%,80%">
+    <FRAME SRC="listMahasiswa.html">
+    <FRAMESET ROWS="20%,80%">
+            <FRAME SRC="header.html">
+            <FRAME SRC="info.html">
+</FRAMESET>
+<FRAME SRC="">
+</FRAMESET>
+<NOFrames>
+    DYWFUTXGDHJGHQJH
+</NOFrames>
+</html>
